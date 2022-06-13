c6b3ecfbbcff36e20828d46b802f621c0563c117
LucasWolfgang <39681420+LucasWolfgang@users.noreply.github.com>
2022-06-13 20:28:56 -0300

    Replacing requirements.txt with setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+   name="QAS Editor",
+   version="0.0.4",
+   description="Question and Answer Sheet editor",
+   author="Lucas Wolfgang",
+   author_email="lucawolfcs@hotmail.com",
+   url="https://github.com/LucasWolfgang/QAS-Editor",
+   packages=["qas_editor"],
+   classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL-3.0 license",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "PyQt5 >= 5.15.0",
+        #"PyPDF2 >= 1.26.0",
+        #"Pillow >= 9.0.0",
+        #"python-docx"
+    ],
+)
