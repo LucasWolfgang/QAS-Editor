5fa782e8878a0f30e8de23304991203b7189e7a7
Wolfgang <39681420+LucasWolfgang@users.noreply.github.com>
2022-10-29 12:40:01 -0300

    Latex parser global updates

--- qas_editor/_parsers/latex.py
+++ qas_editor/_parsers/latex.py
@@ -20,6 +20,7 @@ Copyright, 2016  Benoit Nennig (benoit.nennig@supmeca.fr)
 Distributed under the terms of the GNU General Public License
 See http://www.gnu.org/licenses/gpl.txt for details.
 """
+from __future__ import annotations
 import re
 import os
 import logging
@@ -28,45 +29,161 @@ from ..enums import Numbering
 from ..utils import FText
 from ..answer import Answer
 from ..question import QEssay, QMultichoice
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Type, List, Tuple
 if TYPE_CHECKING:
     from ..category import Category
+    from io import StringIO
 
 _LOG = logging.getLogger(__name__)
-_TEX_CMD = re.compile(r"\\(\w+)(?:\[(.+)\])?\{(.*)\}$")
+_TEX_BASE_CMD = re.compile(r"\\(\w+).*(?:\{(.+)\})")
 
 
-class _ClassExam():
+class _Cmd():
+
+    def __init__(self, name: str):
+        self.name = name
+        self.env = name in ("begin", "end")
+        self.text = None
+        self.args = []
+        self.opts = []
+        self.subitems = []
+
+    def __str__(self) -> str:
+        return self.name + "{" + "}{".join(self.args) + "}[" + "][".join(self.opts) + "]"
+
+
+_TEMPLATES = {}
+class LaTex():
+
+    _NAME = ""
+    _CMDS = {}
+    _QTYPE = {}
+    _HEADER = ""
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        if cls._NAME is not None:
+            _TEMPLATES[cls._NAME] = cls
+
+    def __init__(self, cat: Category, buffer: StringIO, path: str):
+        self.cat = cat
+        self.buf = buffer
+        self.path = path
+        self.idx = 0
+        self.line = "\n"
+
+    def read(self):
+        while self.line:
+            for cmd in self._document():
+                if isinstance(cmd, _Cmd) and cmd.name == "end" and cmd.args[0] == "document":
+                    return
+
+    def _document(self):
+        data = self._parse()
+        for cmd in data:
+            if isinstance(cmd, _Cmd) and cmd.name == "include":
+                path = cmd.args[0]
+                if os.path.relpath(path):
+                    path = self.path.rsplit("/", 1)[0] + "/" + path
+                with open(path) as ifile:
+                    self.__class__(self.cat, ifile, path)
+        return data
+
+    def _parse_cmd(self, open_groups: list):
+        self.idx += 1
+        start = self.idx
+        while self.line[self.idx].isalpha():
+            self.idx += 1
+        cmd = _Cmd(self.line[start: self.idx])
+        while self.idx < len(self.line):
+            if self.line[self.idx] in ("{", "["):
+                open_groups.append(self.idx)
+            elif self.line[self.idx] == "}":
+                if self.line[open_groups[-1]] != "{":
+                    raise ValueError(f"Incorrect pattern: {self.line}")
+                start = open_groups.pop() + 1
+                cmd.args.append(self.line[start:self.idx])
+            elif self.line[self.idx] == "]":
+                if self.line[open_groups[-1]] != "[":
+                    raise ValueError(f"Incorrect pattern: {self.line}")
+                start = open_groups.pop() + 1
+                cmd.opts.append(self.line[start:self.idx])
+            elif self.line[self.idx] == "\\":
+                self._parse_cmd(open_groups)
+            elif len(open_groups) == 0 and self.line[self.idx]:
+                break
+            if self.idx == len(self.line)-1 and len(open_groups) != 0:
+                self.line += next(self.buf).strip()
+            self.idx += 1
+        return cmd
+
+    def _parse(self) -> list:
+        self.idx = start = 0
+        data = []
+        self.line = next(self.buf)
+        while self.idx < len(self.line):
+            if self.line[self.idx] == "\\" and self.line[self.idx+1].isalpha():
+                if self.idx > start + 1:
+                    data.append(self.line[start: self.idx])
+                data.append(self._parse_cmd([]))
+                start = self.idx + 1
+            self.idx += 1
+        data.append(self.line[start: self.idx])
+        return data
+
+
+class _ClassExam(LaTex):
+    """See: https://ctan.org/pkg/exam
     """
-    See: https://ctan.org/pkg/exam
+    _NAME = "exam"
+
+
+class _PkgAMQ(LaTex):
+    """See: https://www.auto-multiple-choice.net/
+    """
+    _NAME = "automultiplechoice"
+
+    _HEADER = """\\documentclass[a4paper]{article}
+        % -------------------------::== package ==::---------------------------
+        \\usepackage[utf8]{inputenc}
+        \\usepackage[T1]{fontenc}
+        \\usepackage{alltt}
+        \\usepackage{multicol}
+        \\usepackage{amsmath,amssymb}
+        \\usepackage{color}
+        \\usepackage{graphicx}
+        % Mandatory for conversion
+        \\usepackage[francais,bloc,completemulti]{automultiplechoice}
+        \\usepackage{tikz}
+        \\usepackage{hyperref}
+        \\usepackage{ulem} % strike text
+
+        % -----------------------::== newcommand ==::--------------------------
+        \\newcommand{\\feedback}[1]{}
+        \\begin{document}
     """
 
 
-class _PkgAMQ():
+class _PkgMcExam(LaTex):
+    """See: https://ctan.org/pkg/mcexam
     """
-    See: https://www.auto-multiple-choice.net/
-    """
+    _NAME = "mcexam"
 
 
-class _PkgMcExam():
+class _PkgAlterQCM(LaTex):
+    """See: https://www.ctan.org/pkg/alterqcm
     """
-    See: https://ctan.org/pkg/mcexam
-    """
+    _NAME = "alterqcm"
 
 
-class _PkgAlterQCM():
-    """
-    See: https://www.ctan.org/pkg/alterqcm
-    """
-
-
-class _PkgGuillaume():
+class _PkgLatexToMoodle(LaTex):
     """Parsers the package created by Guillame for the latextomoodle repo.
     Since document classes have priority, it will only be used if the class of
     the document is not assigned to any parser. This is a LAZY parser! It is
     meant to be simple and fast, and not to consider all possibilities.
     See: https://github.com/Guillaume-Garrigos/moodlexport
     """
+    _NAME = "latextomoodle"
 
     _QTYPE = {
         "multichoice": QMultichoice,
@@ -75,9 +192,9 @@ class _PkgGuillaume():
 
     _CMDS = {
         "title": ("name", str),
-        "generalfeedback": ("feedback", FText),
+        "generalfeedback": ("feedback", "_ftext"),
         "grade": ("default_grade", float),
-        "penalty": ("penalty", float),
+        "penalty": ("max_tries", lambda x: int(1/float(x)) if float(x) != 0 else -1),
         "idnumber": ("dbid", int),
         "responseformat": ("rsp_format", str),
         "responserequired": ("rsp_required", bool),
@@ -88,46 +205,41 @@ class _PkgGuillaume():
         "single": ("single", bool),
         "shuffleanswers": ("shuffle", bool),
         "answernumbering": ("numbering", Numbering),
-        # "correctfeedback": ("if_correct", FText),
-        # "partiallycorrectfeedback": ("if_incomplete", FText),
-        # "incorrectfeedback": ("if_incorrect", FText),
         "shownumcorrect": ("show_ans", bool),
     }
 
-    def __init__(self, cls, cat, buffer) -> None:
-        self.cls = cls
-        self.cat = cat
-        self.buf = buffer
-        self._document()
+    def _ftext(string: str):
+        return FText(string)
 
-    def _question(self, qtype):
+    def _question(self, qtype: str):
         params = {"question": FText()}
         options = []
-        for line in self.buf:
-            tmp = line.strip()
-            if tmp == "\\end{question}":
-                if options:
-                    params["options"] = options
-                break
-            match = _TEX_CMD.match(tmp)
-            if match:
-                cmd, opt, value = match.groups()
-                if cmd == "answer":
-                    options.append(Answer(float(opt), value))
-                elif cmd not in self._CMDS:
-                    continue
+        while self.line:
+            data = self._parse()
+            if data and isinstance(data[0], _Cmd):
+                if data[0].name == "end" and data[0].args[0] == "question":
+                    if options:
+                        params["options"] = options
+                    break
                 else:
-                    key, cast = self._CMDS[cmd]
-                    params[key] = cast(value)
-            else:
-                params["question"].text += tmp if tmp else line
-        params["question"].text = params["question"].text.strip()
+                    value = data[0].args[0]
+                    if data[0].name == "answer":
+                        options.append(Answer(float(data[0].opts[0]), value))
+                    elif data[0].name in self._CMDS:
+                        key, cast = self._CMDS[data[0].name]
+                        if isinstance(cast, str):
+                            params[key] = getattr(self, cast)(value)
+                        else:
+                            params[key] = cast(value)
+            elif self.line:
+                params["question"].text.append(self.line)
+        params["question"].text = params["question"].get().strip() 
         self.cat.add_question(self._QTYPE[qtype](**params))
 
     def _category(self, name):
         tmp = self.cat
         try:
-            self.cat = self.cls(name)
+            self.cat = self.cat.__class__(name)
             tmp.add_subcat(self.cat)
             for line in self.buf:
                 line = line.strip()
@@ -146,141 +258,44 @@ class _PkgGuillaume():
         self.cat = tmp
 
     def _document(self):
-        for line in self.buf:
-            line = line.strip()
-            if line[:16] == "\\begin{category}":
-                self._category(line[17:-1])
-            elif line[:16] == "\\begin{question}":
-                self._question(line[17:-1])
-            elif line == "\\end{document}":
-                break
-            elif line:
-                raise ValueError("Couldnt map line %s", line)
-
-
-_TEMPLATES = {
-    "latextomoodle": _PkgGuillaume,
-    "automultiplechoice": _PkgAMQ,
-    "mcexam": _PkgMcExam,
-    "alterqcm": _PkgAlterQCM,
-    "exam": _ClassExam
-}
-
-
-class Flatex:
-    """ Merge all included tex files in one.
-    """
-
-    TEX_INPUT = re.compile(r"(^[^\%]*\\input{[^}]*})|(^[^\%]*\\include{[^}]*})")
-
-    def __init__(self, base_file: str, noline=False, magic_flag=True):
-        """Create a new Flatex instance.
-        Args:
-            base_file (_type_): Input tex filename.
-            noline (bool, optional): Add blank line after include/input Defaults to False.
-            magic_flag (bool, optional): Remove or not the magic comment tag. Defaults to True.
-        """
-        # store
-        self.base_file = base_file
-        self.noline = noline
-        self.magic_flag = magic_flag
-
-        # define the tag used to prefix the 'magic comments'
-        self.magictag = '%amc2moodle '
-
-        # define state variable for log
-        self._magic_comments_number = 0
-        self._included_files_list = [base_file]
-
-    @staticmethod
-    def _get_input(line):
-        """ Gets the file name from a line containing an input statement.
-        """
-        tex_input_filename_re = r"""{[^}]*"""
-        m = re.search(tex_input_filename_re, line)
-        return m.group()[1:]
-
-    @staticmethod
-    def _combine_path(base_path, relative_ref):
-        """ Return the absolute filename path of the included tex file.
-        """
-        # check for absolute path
-        if not os.path.isabs(relative_ref):
-            abs_path = os.path.join(base_path, relative_ref)
-        else:
-            abs_path = relative_ref
-        # Handle if .tex is supplied directly with file name or not
-        if not relative_ref.endswith('.tex'):
-            abs_path += '.tex'
-        return abs_path
-
-    def _expand_file(self, base_file, current_path):
-        """ Recursively-defined function that takes as input a file and returns
-        it with all the inputs replaced with the contents of the referenced file.
-        """
-        output_lines = []
-        with open(base_file, "r") as f:
-            for line in f:
-                # test if it contains an '\include' or '\input'
-                if self.TEX_INPUT.search(line):
-                    new_base_file = self._combine_path(current_path,
-                                                      self._get_input(line))
-                    output_lines += self._expand_file(new_base_file, current_path)
-                    self._included_files_list.append(new_base_file)
-                    if self.noline:
-                        pass
-                    else:
-                        # add a new line after each inclided file
-                        output_lines.append('\n')
-                # test if magic coment
-                elif self.magic_flag and line.lstrip().startswith(self.magictag):
-                    output_lines += line.replace(self.magictag, '')
-                    # count it
-                    self._magic_comments_number += 1
-                # else append line
-                else:
-                    output_lines.append(line)
-        return output_lines
-
-    def expand(self) -> str:
-        """ This "flattens" a LaTeX document by replacing all \\input{X} lines
-        with the text actually contained in X.
-        """
-        current_path = os.path.split(self.base_file)[0]
-        data = ''.join(self._expand_file(self.base_file, current_path))
-        _LOG.info(f"{self._magic_comments_number} magic comments found, in "
-                  f"{len(self._included_files_list)} tex files.")
+        data = super()._document()
+        if data and isinstance(data[0], _Cmd) and data[0].env:
+            if "category" in data[0].args:
+                self._category(data[0].opts[0])
+            elif "question" in data[0].args:
+                self._question(data[0].opts[0])
+        elif self.line:
+            raise ValueError("Couldnt map line:\n\t%s", self.line)
         return data
 
 
-def read_latex(cls, file_name) -> "Category":
+def read_latex(cls: Type[Category], file_name: str) -> "Category":
     """
     """
     category = cls()
-    category.data["latex"] = []
+    category.metadata["latex"] = []
     tex_class = None
     with open(file_name, 'r', encoding='utf-8') as ifile:
         for line in ifile:
             if tex_class is None:
-                match = _TEX_CMD.match(line.strip())
-                if not match:
-                    continue
-                cmd, opt, value = match.groups()
-                if cmd in ("usepackage", "documentclass"):
-                    category.data["latex"].append((cmd, opt, value))
-                    if value in _TEMPLATES:
-                        tex_class = _TEMPLATES[value]
+                match = _TEX_BASE_CMD.match(line.strip())
+                if match:
+                    cmd, value = match.groups()
+                    category.metadata["latex"].append((cmd, value))
+                    if cmd in ("usepackage", "documentclass"):
+                        if value in _TEMPLATES:
+                            tex_class = _TEMPLATES[value]
             elif line == "\\begin{document}\n":
-                tex_class(cls, category, ifile)
+                tex = tex_class(category, ifile, file_name)
+                tex.read()
+                break
     return category
 
 
-def write_latex(self, file_path: str) -> None:
+def write_latex(self, file_path: str, ftype: type) -> None:
     """_summary_
-
     Args:
         file_path (str): _description_
-
     Raises:
         NotImplementedError: _description_
     """
--- test/test_parser/test_latex.py
+++ test/test_parser/test_latex.py
@@ -16,22 +16,35 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
+from io import StringIO
 import os
 from qas_editor.category import Category
-
+from qas_editor._parsers import latex
 
 TEST_PATH = os.path.dirname(__file__)
 SRC_PATH = os.path.abspath(os.path.join(TEST_PATH, '..'))
 
 
-def test_guillaume_read():
-    EXAMPLE = f"{TEST_PATH}/datasets/latex/guillaume_read.tex"
+def test_raw_multargs():
+    tmp = StringIO("\\acommand[key1=value1, key2=value2]{a_value}"
+                   "{another_value}[opt1]{final_value}[opt2]")
+    cat = Category()
+    tex = latex.LaTex(cat, tmp, "")
+    data = tex._document()
+    assert len(data) == 1
+    assert data[0].name == "acommand"
+    assert data[0].args == ["a_value", "another_value", "final_value"]
+    assert data[0].opts == ["key1=value1, key2=value2", "opt1", "opt2"]
+
+
+def test_latextomoodle_read():
+    EXAMPLE = f"{TEST_PATH}/datasets/latex_l2m/read.tex"
     control = Category.read_latex(EXAMPLE)
-    control = control["My little catgory from latex"]
+    control = control["My little category from latex"]
     assert len(control) == 1
     assert control.get_size(False) == 3
     assert control.get_size(True) == 4
-    assert control.name == "My little catgory from latex"
+    assert control.name == "My little category from latex"
     question = next(control["Simple arithmetic"].questions)
     assert question.question.get() == "The product 6x8 is equal to ... :"
     opts = question.options
@@ -40,7 +53,7 @@ def test_guillaume_read():
     assert opts[0].fraction == 0.0
 
 
-def test_guillaume_vs_moodle():
+def test_latextomoodle_vs_moodle():
     EXAMPLE = f"{TEST_PATH}/datasets/latex/guillaume_xml.tex"
     data = Category.read_latex(EXAMPLE)
     data.metadata.clear()   # This will always be different
