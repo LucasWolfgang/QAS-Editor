7dbba2fa173b5cfda11640aa256b93a794e3e883
lucawolfcs@hotmail.com <39681420+LucasWolfgang@users.noreply.github.com>
2023-05-20 00:53:00 -0300

    Updated QTI/IMS datasets and testcases

--- test/test_parser/test_qti.py
+++ test/test_parser/test_qti.py
@@ -15,17 +15,37 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-
 import os
-from qas_editor._parsers import qti1v2, bb
+import shutil
+import pytest
+from qas_editor._parsers.ims import qti1v2, bb, IMS, canvas
 from qas_editor.category import Category
 
 TEST_PATH = os.path.dirname(os.path.dirname(__file__))
 
+@pytest.fixture(scope="module")
+def all_question_types():
+    EXAMPLE = f"{TEST_PATH}/datasets/ims/canvas_all_question_types.imscc"
+    TMP = f"{TEST_PATH}/datasets/ims/canvas_all_question_types_tmp"
+    shutil.rmtree(TMP, ignore_errors=True)
+    yield IMS(EXAMPLE, TMP)
+    shutil.rmtree(TMP)
+
+def test_read_manifest(all_question_types: IMS):
+    all_question_types.get_manifest()
+
+
+def test_read_canvas():
+    EXAMPLE = f"{TEST_PATH}/datasets/ims/canvas_all_question_types.imscc"
+    TMP = f"{TEST_PATH}/datasets/ims/canvas_all_question_types_tmp"
+    cat = Category()
+    shutil.rmtree(TMP, ignore_errors=True)
+    canvas.read_cc_canvas(cat, EXAMPLE)
 
 def test_read():
     EXAMPLE = f"{TEST_PATH}/datasets/qti1v2/item.zip"
-    parser = qti1v2.QTIParser1_2()
+    parser = qti1v2.QTIParser1v2()
+    parser.read(EXAMPLE)
     
 
 def test_write_bb():
