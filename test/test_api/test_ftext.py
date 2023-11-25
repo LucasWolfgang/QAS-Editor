049bfdfd138ea4e306883a91195046191e3752d3
lucawolfcs@hotmail.com <39681420+LucasWolfgang@users.noreply.github.com>
2023-11-25 18:28:35 -0300

    Updated headers of the test files

--- test/test_api/test_ftext.py
+++ test/test_api/test_ftext.py
@@ -1,19 +1,21 @@
+# Question and Answer Sheet Editor <https://github.com/LucasWolfgang/QAS-Editor>
+# Copyright (C) 2022  Lucas Wolfgang
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-Question and Answer Sheet Editor <https://github.com/LucasWolfgang/QAS-Editor>
-Copyright (C) 2022  Lucas Wolfgang
+## Description
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 import os
 
@@ -134,7 +136,7 @@ def test_moodle_ascii_embeeded():
         "{=sqrt(({x}-{y})*({x}+{y}))}</li><li>'pi' is a function in moodle,"
         " {=sin(1.5*pi())}</li><li>test with '- unary' expression"
         " {=-{x}+(-{y}+2)}<br/></li></ul>Something outside")
-    parser = MoodleXHTMLParser(True, False, None)
+    parser = MoodleXHTMLParser(True, True, None)
     parser.parse(s)
     assert parser.pos == 17
     ftext = FText(parser)
--- test/test_parser/test_csv_card.py
+++ test/test_parser/test_csv_card.py
@@ -1,40 +1,51 @@
-""""
-Question and Answer Sheet Editor <https://github.com/LucasWolfgang/QAS-Editor>
-Copyright (C) 2022  Lucas Wolfgang
+# Question and Answer Sheet Editor <https://github.com/LucasWolfgang/QAS-Editor>
+# Copyright (C) 2022  Lucas Wolfgang
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+## Description
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import os
-from qas_editor.category import Category
+
+from qas_editor import category, enums
 
 TEST_PATH = os.path.dirname(os.path.dirname(__file__))
 
 
-def _diff_file(file_name: str):
-    control = Category.read_csvcard(file_name)
-    XML_TEST = f"{file_name}.tmp"
-    control.write_csvcard(XML_TEST)
-    new_data = Category.read_csvcard(XML_TEST)
+def test_read_all():
+    """_summary_
+    """
+    file_name = f"{TEST_PATH}/datasets/anki/math.txt"
+    lang = enums.Language.EN_US
+    control = category.Category.read_csvcard(file_name, lang)
+    assert control.get_size(False) == 91
+    assert control.get_size(True) == 91
+    qst = control.get_question(10)
+    assert qst.body[lang].get() == '2 x 1 ='
+
+
+def test_diff_simple():
+    """_summary_
+    """
+    file_name = f"{TEST_PATH}/datasets/anki/math.txt"
+    tmp_test = f"{file_name}.tmp"
+    lang = enums.Language.EN_US
+    control = category.Category.read_csvcard(file_name, lang)
+    control.write_csvcard(tmp_test)
+    new_data = category.Category.read_csvcard(tmp_test)
     assert control.compare(new_data, [])
-    os.remove(XML_TEST)
+    os.remove(tmp_test)
     return control
-
-
-def test_read_anki():
-    cat = _diff_file(f"{TEST_PATH}/datasets/anki/math.txt")
-    assert cat.get_size(False) == 91
-    assert cat.get_size(True) == 91
-    qst = cat.get_question(10)
-    assert qst.question.get() == '2 x 1 ='
